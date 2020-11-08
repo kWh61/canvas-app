c3977e71a3683df957d57e106e540e9d5c629916
kwang457 <kwang457@gatech.edu>
2020-11-08 12:12:57 -0500

    Initial commit

---
+++ src/Canvas.tsx
@@ -0,0 +1,14 @@
+import React from 'react';
+import styles from './Canvas.module.css';
+
+interface CanvasProps {
+  
+};
+
+const Canvas: React.FC<CanvasProps> = () => {
+  return (
+    <canvas></canvas>
+  );
+};
+
+export default Canvas;
